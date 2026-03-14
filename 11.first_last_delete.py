67b26e07536eac466c4cb176df0410f8b5670970
Shamanesss <mentulbenodorna@gmail.com>
2026-03-14 09:57:39 +0100

    añadido ejercicio first_last_delete

---
+++ 11.first_last_delete.py
@@ -0,0 +1,6 @@
+def remove_first_and_last(list_to_clean):
+    languages = ["Python", "Java", "C++", "JavaScript", "Ruby", "Swift", "Go", "Rust"]
+    list_clean = languages[1:-1]
+    print(list_clean)
+
+remove_first_and_last(["Python", "Java", "C++", "JavaScript", "Ruby", "Swift", "Go", "Rust"])
