4a4f5c57237afc9e5399e024e754fd9f7e48f90c
Shamanesss <mentulbenodorna@gmail.com>
2026-03-15 10:59:45 +0100

    añadido first last delete

--- 11.first_last_delete.py
+++ 11.first_last_delete.py
@@ -4,3 +4,13 @@ def remove_first_and_last(list_to_clean):
     print(list_clean)
 
 remove_first_and_last(["Python", "Java", "C++", "JavaScript", "Ruby", "Swift", "Go", "Rust"])
+# otra opcion es usar el operador *
+
+def remove_first_and_last_II(list_to_clean):
+    _, *content, _ = list_to_clean
+    print(list_to_clean)
+    return content
+html = ['<h1>', 'My content', '</h1>']
+
+print(remove_first_and_last_II(html))
+
